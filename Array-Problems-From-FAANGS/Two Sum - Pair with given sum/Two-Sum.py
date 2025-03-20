e5bf3a54aab0a4f0322e98cb95a2807961ddc074
Mohamed Elshamy <113453255+Mohamed-Sobhy-Elshamy@users.noreply.github.com>
2025-03-21 00:44:57 +0200

    Add Two Problem
    Two Problems is => (Two sum - Pair with given sum) and (Majority Element)

---
+++ Array-Problems-From-FAANGS/Two Sum - Pair with given sum/Two-Sum.py
@@ -0,0 +1,18 @@
+arr = [3, 1, 3, 3, 2]
+
+def majorityElement(arr):
+    count_map = {}
+
+    for num in arr:
+        if num in count_map:
+            count_map[num] += 1
+        else: 
+            count_map[num] = 1
+
+    n = len(arr)
+    for key, value in count_map.items():
+        if value > n // 2:
+            return key 
+    return -1
+
+print(majorityElement(arr))
